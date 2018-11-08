589e8d11ee6d59d7ca1a6d9aee121fd4b871fab6
Henry Fock <h_fock01@wwu.de>
2018-11-08 12:59:53 +0100

    CLI Tool GeoTIFF auslesen funktioniert
    Auslesen der Boundingbox einer GeoTIFF funktioniert und gibt die koordinaten in WGS84 zurück

--- CLI Tools/getBoundingBox.py
+++ CLI Tools/getBoundingBox.py
@@ -15,6 +15,7 @@ import pygeoj
 from netCDF4 import Dataset
 from osgeo import gdal
 from osgeo import ogr
+from osgeo import osr
 import pandas as pd
 import csv
 
@@ -27,7 +28,7 @@ def getBoundingBox(name, path):
     filename, file_extension = os.path.splitext(filepath)
     print(file_extension)
     #shapefile handelig
-    if file_extension == ".shp" or file_extension == ".dbf":
+    if file_extension == ".shp":
         try:
             myshp = open(filepath, "rb")
             sf = shapefile.Reader(shp=myshp)
@@ -61,11 +62,48 @@ def getBoundingBox(name, path):
             return None
 
     elif file_extension == ".tif":
+        # @see https://stackoverflow.com/questions/2922532/obtain-latitude-and-longitude-from-a-geotiff-file
         try:
-            gtif = gdal.Open(filepath)
-            print (gtif.GetMetadata())
+            # get the existing coordinate system
+            ds = gdal.Open(filepath)
+            old_cs= osr.SpatialReference()
+            old_cs.ImportFromWkt(ds.GetProjectionRef())
+
+            # create the new coordinate system
+            wgs84_wkt = """
+            GEOGCS["WGS 84",
+                DATUM["WGS_1984",
+                    SPHEROID["WGS 84",6378137,298.257223563,
+                        AUTHORITY["EPSG","7030"]],
+                    AUTHORITY["EPSG","6326"]],
+                PRIMEM["Greenwich",0,
+                    AUTHORITY["EPSG","8901"]],
+                UNIT["degree",0.01745329251994328,
+                    AUTHORITY["EPSG","9122"]],
+                AUTHORITY["EPSG","4326"]]"""
+            new_cs = osr.SpatialReference()
+            new_cs .ImportFromWkt(wgs84_wkt)
+
+            # create a transform object to convert between coordinate systems
+            transform = osr.CoordinateTransformation(old_cs,new_cs) 
+
+            #get the point to transform, pixel (0,0) in this case
+            width = ds.RasterXSize
+            height = ds.RasterYSize
+            gt = ds.GetGeoTransform()
+            minx = gt[0]
+            miny = gt[3] + width*gt[4] + height*gt[5] 
+            maxx = gt[0] + width*gt[1] + height*gt[2]
+            maxy = gt[3] 
+
+            #get the coordinates in lat long
+            latlongmin = transform.TransformPoint(minx,miny)
+            latlongmax = transform.TransformPoint(maxx,maxy)
+            bbox = [latlongmin[0], latlongmin[1], latlongmax[0], latlongmax[1]]
+            click.echo(bbox)
+            return bbox
         except:
-            click.echo("File not Found")
+            click.echo("File not Found or TIFF is not GeoTIFF")
 
     elif file_extension == ".nc":
         rootgrp = Dataset(filepath, "w", format="NETCDF4")
