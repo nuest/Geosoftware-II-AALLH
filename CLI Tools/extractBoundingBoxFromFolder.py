7538848c2a07bc268e3228576e8bb8c8ed2ec7fd
Henry Fock <h_fock01@wwu.de>
2018-11-30 12:08:41 +0100

    nebenläufige Extraktion möglich
    Nur wenn man einen Ordner Extrahiert

--- CLI Tools/extractBoundingBoxFromFolder.py
+++ CLI Tools/extractBoundingBoxFromFolder.py
@@ -1,6 +1,12 @@
+"""
+@Author Henry Fock
+"""
+
+
 import getBoundingBox as box
 import getTimeExtent as timeEx
 import subprocess
+import threading
 
 from os import listdir
 from os.path import isfile, join
@@ -9,10 +15,23 @@ import click
 @click.command()
 @click.option('--path', prompt="File path", help='Path to file')
 def main(path):
-    res = getAllResultsFromCLIFromFolder(path)
+    threads = []
+    res = []
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+    for name in onlyfiles:
+        current = CliTools(path, name)
+        threads.append(current)
+        current.start()
+
+    for thread in threads:
+        thread.join()
+        res.append(thread.result)
+
+    click.echo("\n Ergebnis:")
     for x in res:
         if x[1][0] == None:
-            if x[2][0]:
+            if x[2][0] == None:
                 click.echo((x[0], x[1][1], x[2][1]))
             else:
                 click.echo((x[0], x[1][1], x[2][0]))
@@ -23,15 +42,18 @@ def main(path):
                 click.echo((x[0], x[1][0], x[2][0]))
 
 
-def getAllResultsFromCLIFromFolder(path):
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    # print(onlyfiles)
-    res = []
-    for fileFromDir in onlyfiles:
-        resultFromFile = [fileFromDir,box.getBoundingBox(path=path, name=fileFromDir), timeEx.getTimeExtend(path=path, name=fileFromDir)]
-        res.append(resultFromFile)
 
-    return res
+class CliTools(threading.Thread):
+    def __init__(self, path, name): 
+        threading.Thread.__init__(self) 
+        self.path = path
+        self.name = name
+        self.result = []
+
+    def run(self):
+        bbox = box.getBoundingBox(path=self.path, name=self.name)
+        time = timeEx.getTimeExtend(path=self.path, name=self.name)
+        self.result.extend([self.name, bbox, time])
 
 
 if __name__ == '__main__':
