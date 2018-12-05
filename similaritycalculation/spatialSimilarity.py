701d6222e0f9cb317fdcf48d5942919b973615ae
Henry Fock <h_fock01@wwu.de>
2018-12-05 13:27:54 +0100

    Punkte mit Polygone können verglichen werden

--- similaritycalculation/spatialSimilarity.py
+++ similaritycalculation/spatialSimilarity.py
@@ -17,6 +17,14 @@ def spatialOverlap(bboxA, bboxB):
     areaA = boxA.GetArea()
     areaB = boxB.GetArea()
 
+    if (areaA == 0) and (areaB == 0):
+        bufferDist = 328 # foot
+        boxA = boxA.Buffer(bufferDist)
+        boxB = boxB.Buffer(bufferDist)
+
+        areaA = boxA.GetArea()
+        areaB = boxB.GetArea()
+
     print(areaA)
 
     largerArea = areaA if areaA >= areaB else areaB
@@ -63,13 +71,24 @@ def similarArea(bboxA, bboxB):
 def spatialDistance(bboxA, bboxB):
     distBetweenCenterPoints = None
     longerDistance = None
-    if (bboxA[1:] == bboxA[:-1]) and (bboxB[1:] == bboxB[:-1]):
+    if (bboxA[0] == bboxA[2]) and (bboxB[0] == bboxB[2]) and (bboxA[1] == bboxA[3]) and (bboxB[1] == bboxB[3]):
         distBetweenCenterPoints = _getDistance((bboxA[0], bboxA[1]),(bboxB[0], bboxB[1]))
         longerDistance = 5
 
-    else:    
-        centerA = _getMidPoint(bboxA)
-        centerB = _getMidPoint(bboxB)
+    else:
+        if (bboxA[0] == bboxA[2]) and (bboxA[1] == bboxA[3]):
+            centerA = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (bboxA[0], bboxA[1]))
+        else:
+            centerA = _getMidPoint(bboxA)
+
+        if (bboxB[0] == bboxB[2]) and (bboxB[1] == bboxB[3]):
+            centerB = ogr.CreateGeometryFromWkt("POINT (%f %f)" % (bboxB[0], bboxB[1]))
+        else:
+            centerB = _getMidPoint(bboxB)
+
+        type1 = centerA.GetGeometryName()
+        type2 = centerB.GetGeometryName()
+        print(type1, type2)
 
         distA = _getDistance((bboxA[1], bboxA[0]), (bboxA[3], bboxA[2]))
         distB = _getDistance((bboxB[1], bboxB[0]), (bboxB[3], bboxB[2]))
@@ -154,20 +173,43 @@ def _getMidPoint(bbox):
     line2 = ogr.CreateGeometryFromWkt(line2)
 
     intersectionPoint = line1.Intersection(line2)
+    z = intersectionPoint.ExportToWkt()
     intersectGeometry = ogr.CreateGeometryFromWkt(intersectionPoint.ExportToWkt())
 
-    # print(intersectGeometry)
+    datatype = intersectGeometry.GetGeometryName()
+
+    if str.upper(datatype) == "LINESTRING":
+        if bbox[1] == bbox[3]:
+            line1 = "LINESTRING (%f %f, %f %f)" % (bbox[0], bbox[1]-0.001, bbox[2], bbox[3]+0.001)
+            line2 = "LINESTRING (%f %f, %f %f)" % (bbox[2], bbox[1]-0.001, bbox[0], bbox[3]+0.001)
+
+        elif bbox[0] == bbox[2]:
+            line1 = "LINESTRING (%f %f, %f %f)" % (bbox[0]-0.001, bbox[1], bbox[2]+0.001, bbox[3])
+            line2 = "LINESTRING (%f %f, %f %f)" % (bbox[2]-0.001, bbox[1], bbox[0]+0.001, bbox[3])
+
+        line1 = ogr.CreateGeometryFromWkt(line1)
+        line2 = ogr.CreateGeometryFromWkt(line2)
+
+        intersectionPoint = line1.Intersection(line2)
+        intersectGeometry = ogr.CreateGeometryFromWkt(intersectionPoint.ExportToWkt())
+
+        datatype2 = intersectGeometry.GetGeometryName()
+
+
     return intersectGeometry
 
+# Geometry
+# bbox1 = [13.0078125, 50.62507306341435, 5.44921875, 45.82879925192134]
+# bbox2 = [17.7978515625, 52.09300763963822, 7.27294921875, 46.14939437647686]
+# spatialDistance(bbox1, bbox2)
 
+# Points
+# bbox1 = [11.0078125, 50.62507306341435, 11.0078125, 50.62507306341435]
+# bbox2 = [13.0082125, 50.62513301341435, 13.0082125, 50.62513301341435]
+# spatialDistance(bbox1, bbox2)
 
-bbox1 = [13.0078125, 50.62507306341435, 5.44921875, 45.82879925192134]
-bbox2 = [17.7978515625, 52.09300763963822, 7.27294921875, 46.14939437647686]
-spatialDistance(bbox1, bbox2)
+# Polygon and Point
+# bbox1 = [11.0078125, 50.62507306341435, 13.0078125, 50.62507306341435]
+# bbox2 = [13.0082125, 50.62513301341435, 13.0082125, 50.62513301341435]
+# spatialDistance(bbox1, bbox2)
 
-
-# _getDistance(point1, point2)
-
-# wkt = "POLYGON ((1162440.5712740074 672081.4332727483, 1162440.5712740074 647105.5431482664, 1195279.2416228633 647105.5431482664, 1195279.2416228633 672081.4332727483, 1162440.5712740074 672081.4332727483))"
-# poly = ogr.CreateGeometryFromWkt(wkt)
-# print ("Area = %d" % poly.GetArea())
